4eac89840437dfe1876a6304a62ec4bd6e328724
David <arattanasack@gmail.com>
2020-01-12 03:21:10 -0800

    Adds score values to everyone

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,25 +1,28 @@
-let friends = [{
-    name: 'Wraith',
-    photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjewPyQ2_3mAhXRLH0KHf4PB3gQjRx6BAgBEAQ&url=https%3A%2F%2Ffirstplayer.fandom.com%2Fwiki%2FWraith_(Apex_Legends)&psig=AOvVaw0ewoys_ArTUt8hmHj_tUtp&ust=1578906428506877',
-    score: []
-}, {
-    name: 'Mirage',
-    photo: 'https://danbooru.donmai.us/data/sample/sample-3f48b21c0ecdc62f533687acb7a52e01.jpg',
-    score: []
-}, {
-    name: 'Lifeline',
-    photo: 'https://static.zerochan.net/Lifeline.%28Apex.Legends%29.full.2534104.jpg',
-    score: []
-}, {
-    name: 'Bloodhound',
-    photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkAFco8fL9Bx7KAVMw_YxVitzoJcgh9NMbbUPIGrNIBkqj4eUlTQ&s',
-    score: []
-}, {
-    name: 'Crypto',
-    photo: 'https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/09/season-3/apex-hero-medium-legends-concept-crypto-7x2-xl.jpg.adapt.crop3x5.320w.jpg',
-    score: []
-}, {
-    name: 'Wattson',
-    photo: 'https://i.redd.it/atdtgiphq8831.png',
-    score: []
-}]
+let friends = [
+    {
+        name: 'Wraith',
+        photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjewPyQ2_3mAhXRLH0KHf4PB3gQjRx6BAgBEAQ&url=https%3A%2F%2Ffirstplayer.fandom.com%2Fwiki%2FWraith_(Apex_Legends)&psig=AOvVaw0ewoys_ArTUt8hmHj_tUtp&ust=1578906428506877',
+        score: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    }, {
+        name: 'Mirage',
+        photo: 'https://danbooru.donmai.us/data/sample/sample-3f48b21c0ecdc62f533687acb7a52e01.jpg',
+        score: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    }, {
+        name: 'Lifeline',
+        photo: 'https://static.zerochan.net/Lifeline.%28Apex.Legends%29.full.2534104.jpg',
+        score: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    }, {
+        name: 'Bloodhound',
+        photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkAFco8fL9Bx7KAVMw_YxVitzoJcgh9NMbbUPIGrNIBkqj4eUlTQ&s',
+        score: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    }, {
+        name: 'Crypto',
+        photo: 'https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/09/season-3/apex-hero-medium-legends-concept-crypto-7x2-xl.jpg.adapt.crop3x5.320w.jpg',
+        score: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    }, {
+        name: 'Wattson',
+        photo: 'https://i.redd.it/atdtgiphq8831.png',
+        score: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    }
+];
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,29 +12,48 @@ module.exports = (app) => {
             photo: '',
             difference: 1000
         };
-        
+
         let userData = req.body;
         let userName = userData.name;
         let userScores = userData.scores;
 
         let matchScore = userScores.map((item) => parseInt(item, 10));
 
-        let matchData = {
+        userData = {
             name: req.body.name,
             photo: req.body.photo,
             scores: matchScore
         };
-        
-        console.log(`/n/r/n==========Name: ${userName}\nUser Score: ${userScores}/n/r/n==========`);
+
+        console.log(`\n\r\n==========Name: ${userName}\nUser Score: ${userScores}\n\r\n==========`);
 
         let sum = matchScore.reduce((a, b) => a + b, 0)
         console.log(`/n/r/n==========Sum of user's score: ${sum}\nBest match score difference: ${match.difference}/n/r/n==========`);
 
-        friends.forEach(element => {
-            console.log(friends.name);
-            scoreDifference = 0;
-            
-        })
+        for (let i = 0; i < friends.length; i++) {
 
-    })
+            console.log(friends[i].name);
+            scoreDifference = 0;
+            console.log(`Total Difference: ${scoreDifference}`);
+            console.log(`Legend Match: ${match.difference}`);
+
+            let legendMatch = friends[i].score.reduce((a, b) => a + b, 0);
+            console.log(`Total friend score: ${legendMatch}`);
+            scoreDifference += Math.abs(sum - matchScore);
+            console.log("-------------------------> " + scoreDifference);
+
+            if (scoreDifference <= match.difference) {
+                match.name = friends[i].name;
+                match.photo = friends[i].photo;
+                match.difference = scoreDifference;
+            }
+            
+            console.log(`Score difference: ${scoreDifference}`);
+        }
+        console.log(match);
+
+        friends.push(userData);
+        console.log(`New entry added!\n----------\n${userData}`);
+        res.json(userData);
+    });
 }
