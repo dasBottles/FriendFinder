de329205ac86c916c18513e30a49d68a8fc286ed
kyuu <arattanasack@gmail.com>
2020-01-12 19:00:20 -0800

    [WIP] Adjust api-routing logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,54 +6,49 @@ module.exports = (app) => {
     });
 
     app.post('/api/friends', (req, res) => {
-        let scoreDifference = 0;
+        let matchDifference = 0;
         let match = {
             name: '',
             photo: '',
-            difference: 1000
+            matchDifference: 1000
         };
 
-        let userData = req.body;
-        let userName = userData.name;
-        let userScores = userData.scores;
-
-        let matchScore = userScores.map((item) => parseInt(item, 10));
-
+        let userData = {
+            name: '',
+            photo: '',
+            scores: []
+        }
+        let userSum = req.body.scores.map((item) => parseInt(item, 10));
+        
         userData = {
             name: req.body.name,
             photo: req.body.photo,
-            scores: matchScore
+            scores: userSum
         };
+        
+        console.log(`\n\r\n==========\nName: ${userData.name}\nUser Score: ${userData.scores}\n\==========`);
+        
+        const scoreTotal = (arr) => arr.reduce((a, b) => a + b, 0);
+        console.log('scoreTotal', scoreTotal(userSum))
 
-        console.log(`\n\r\n==========Name: ${userName}\nUser Score: ${userScores}\n\r\n==========`);
+        friends.forEach(element => {
+            difference = 0;
+            let matchSum = scoreTotal(element.score);
+            console.log(`\n------------\nLegend Name: ${element.name}\nScore: ${matchSum}\n------------\n`);
 
-        let sum = matchScore.reduce((a, b) => a + b, 0)
-        console.log(`\n\r\n==========Sum of user's score: ${sum}\nBest match score difference: ${match.difference}\n\r\n==========`);
+            difference += Math.abs(matchSum - userSum);
 
-        for (let i = 0; i < friends.length; i++) {
-
-            console.log(friends[i].name);
-            scoreDifference = 0;
-            console.log(`Total Difference: ${scoreDifference}`);
-            console.log(`Legend Match: ${match.difference}`);
-
-            let legendMatch = friends[i].score.reduce((a, b) => a + b, 0);
-            console.log(`Total friend score: ${legendMatch}`);
-            scoreDifference += Math.abs(sum - matchScore);
-            console.log("-------------------------> " + scoreDifference);
-
-            if (scoreDifference <= match.difference) {
-                match.name = friends[i].name;
-                match.photo = friends[i].photo;
-                match.difference = scoreDifference;
+            if (difference <= matchDifference) {
+                matchDifference = difference;
+                match.name = element.name;
+                match.photo = element.photo;
             }
-            
-            console.log(`Score difference: ${scoreDifference}`);
-        }
-        console.log(match);
+        });
+
+
 
         friends.push(userData);
-        console.log(`New entry added!\n----------\n${userData}`);
         res.json(userData);
+        console.log(`New entry added!\n----------\n${userData}`);
     });
 }
