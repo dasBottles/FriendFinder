ca57d740899d2ff6e74304bed1ac351369e2ebac
David <arattanasack@gmail.com>
2020-01-13 22:54:23 -0800

    Adjust survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,15 +2,15 @@ let friends = [
     {
         name: 'Wraith',
         photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjewPyQ2_3mAhXRLH0KHf4PB3gQjRx6BAgBEAQ&url=https%3A%2F%2Ffirstplayer.fandom.com%2Fwiki%2FWraith_(Apex_Legends)&psig=AOvVaw0ewoys_ArTUt8hmHj_tUtp&ust=1578906428506877',
-        score: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+        score: [5, 1, 4, 4, 5, 1, 2, 5, 4, 5]
     }, {
         name: 'Mirage',
         photo: 'https://danbooru.donmai.us/data/sample/sample-3f48b21c0ecdc62f533687acb7a52e01.jpg',
-        score: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+        score: [4, 2, 5, 3, 1, 4, 2, 1, 3, 2]
     }, {
         name: 'Lifeline',
         photo: 'https://static.zerochan.net/Lifeline.%28Apex.Legends%29.full.2534104.jpg',
-        score: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+        score: [5, 2, 2, 2, 4, 5, 3, 2, 5, 4]
     }, {
         name: 'Bloodhound',
         photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkAFco8fL9Bx7KAVMw_YxVitzoJcgh9NMbbUPIGrNIBkqj4eUlTQ&s',
@@ -18,7 +18,7 @@ let friends = [
     }, {
         name: 'Crypto',
         photo: 'https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/09/season-3/apex-hero-medium-legends-concept-crypto-7x2-xl.jpg.adapt.crop3x5.320w.jpg',
-        score: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+        score: [4, 3, 4, 1, 5, 2, 3, 3, 1, 3]
     }, {
         name: 'Wattson',
         photo: 'https://i.redd.it/atdtgiphq8831.png',
